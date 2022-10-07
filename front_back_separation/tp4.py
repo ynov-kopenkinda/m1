e2fb475ff558d363f8831f8b8cf64306aee13952
Dmitrii <kopenkin.da@gmail.com>
2022-10-07 10:29:21 +0200

    fixed

--- front_back_separation/tp4.py
+++ front_back_separation/tp4.py
@@ -4,7 +4,8 @@ while True:
     number = input("Entrez un nombre entier (ou 'q' pour quitter): ")
     if number == "q":
         break
-    if number not in numbers:
-        numbers.append(int(number))
+    if int(number) in numbers:
+        continue
+    numbers.append(int(number))
 
 print("Liste des nombres: {}".format(numbers))
