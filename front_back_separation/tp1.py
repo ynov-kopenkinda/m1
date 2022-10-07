398ea9f148451f7abeaefc5fee8dfe900b18f7b0
Dmitrii <kopenkin.da@gmail.com>
2022-10-07 09:27:48 +0200

    tp1 done

---
+++ front_back_separation/tp1.py
@@ -0,0 +1,8 @@
+a = 3
+b = 12
+c = 4
+print("Les trois variables sont a = {}, b = {}, c = {}".format(a, b, c))
+somme = a + b + c
+print("La somme des trois variables est {}".format(somme))
+a, b = b, a
+print("a vaut maintenant = {}, b = {}".format(a, b))
