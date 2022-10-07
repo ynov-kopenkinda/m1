d9a8870927bf1a81dffe77a8199345068124f745
Dmitrii <kopenkin.da@gmail.com>
2022-10-07 11:23:42 +0200

    ex6

---
+++ front_back_separation/tp6.py
@@ -0,0 +1,15 @@
+import random
+
+temps_list = [random.randint(18, 25) for _ in range(30)]
+temps_dict = {}
+
+
+for temp in temps_list:
+    t = "{}°C".format(temp)
+    if temps_dict.get(t) is not None:
+        temps_dict[t] += 1
+    else:
+        temps_dict[t] = 1
+
+for temp, count in temps_dict.items():
+    print("{}: {}".format(temp, count))
