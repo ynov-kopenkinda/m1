b1929393eb5105ad49cd9f7fc3104b2f196f44c3
Dmitrii <kopenkin.da@gmail.com>
2022-10-14 09:09:26 +0200

    done tp

---
+++ front_back_separation/14-10-2022/tp12.py
@@ -0,0 +1,40 @@
+def menu():
+    print("1. Ajouter une note")
+    print("2. Afficher les notes")
+    print("3. Afficher une note")
+    print("4. Calculer la moyenne")
+    print("q. Quitter")
+    choix = input("Votre choix : ")
+    if choix not in ['1', '2', '3', '4', 'q']:
+        print("Choix invalide")
+        return menu()
+    return choix
+
+
+notes = []
+
+while True:
+    choix = menu()
+    if choix == 'q':
+        break
+    elif choix == '1':
+        try:
+            note = int(input("Note : "))
+            notes.append(note)
+        except ValueError:
+            print("Veuiilez entrer un nombre")
+            continue
+    elif choix == '2':
+        print(notes)
+    elif choix == '3':
+        try:
+            index = int(input("Index : "))
+            print(notes[index])
+        except ValueError:
+            print("Veuiilez entrer un nombre")
+            continue
+        except IndexError:
+            print("Index invalide")
+            continue
+    elif choix == '4':
+        print(sum(notes) / len(notes))
