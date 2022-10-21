8cea2fb6d85406200256981a7a039ecbd76feddc
Dmitrii <kopenkin.da@gmail.com>
2022-10-21 13:53:23 +0200

    migrations + install

---
+++ front_back_separation/21-10-2022/django_1/artists/myapp1/models.py
@@ -0,0 +1,23 @@
+from typing import List, Tuple
+from django.db import models
+
+# Create your models here.
+
+
+class Artist(models.Model):
+    STYLE: List[Tuple[str, str]] = [
+        ("POP", "Pop"),
+        ("RAP", "Rap"),
+        ("CLS", "Classic"),
+        ("RCK", "Rock"),
+        ("UND", "Undefined"),
+    ]
+    name = models.CharField(max_length=100)
+    style = models.CharField(max_length=3, choices=STYLE, default="UND")
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    duration = models.DurationField()
+    release_date = models.DateField()
