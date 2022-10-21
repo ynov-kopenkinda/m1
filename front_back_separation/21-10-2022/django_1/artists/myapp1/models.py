954c712e83bf9338da594f5fea0002bac965908d
Dmitrii <kopenkin.da@gmail.com>
2022-10-21 14:07:20 +0200

    pre-drf

--- front_back_separation/21-10-2022/django_1/artists/myapp1/models.py
+++ front_back_separation/21-10-2022/django_1/artists/myapp1/models.py
@@ -15,9 +15,15 @@ class Artist(models.Model):
     name = models.CharField(max_length=100)
     style = models.CharField(max_length=3, choices=STYLE, default="UND")
 
+    def __str__(self):
+        return self.name
+
 
 class Song(models.Model):
     title = models.CharField(max_length=100)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     duration = models.DurationField()
     release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
