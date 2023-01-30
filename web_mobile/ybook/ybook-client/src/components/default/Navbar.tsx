016c18bac46b426d8e9547ba620a4fda80efb203
Dmitrii <kopenkin.da@gmail.com>
2023-01-30 15:07:44 +0100

    Update notification preview on navbar

--- web_mobile/ybook/ybook-client/src/components/default/Navbar.tsx
+++ web_mobile/ybook/ybook-client/src/components/default/Navbar.tsx
@@ -8,10 +8,12 @@ import {
 import cx from "classnames";
 import { Link, useLocation } from "react-router-dom";
 import { useSession } from "../../hooks/auth/useSession";
+import { useNotifications } from "../../hooks/notifications/useNotifications";
 import { Avatar } from "./Avatar";
 
 export function AppNavbar() {
   const { data: session, status } = useSession();
+  const [notifications] = useNotifications();
   return (
     <div className="fixed left-2 right-2 bottom-2 flex h-16 items-center justify-between rounded-full border bg-white/75 px-4 backdrop-blur-sm">
       <NavbarIcon to="/" icon={<IconHome stroke={1} />} />
@@ -19,7 +21,19 @@ export function AppNavbar() {
       <NavbarIcon to="/new-post" icon={<IconPlus stroke={1} />} />
       <NavbarIcon to="/messages" icon={<IconMessage stroke={1} />} />
       {status === "success" && (
-        <NavbarIcon to="/settings" icon={<Avatar user={session.user} />} />
+        <NavbarIcon
+          to="/settings"
+          icon={
+            <>
+              {notifications !== undefined && notifications?.length > 0 && (
+                <span className=" absolute -top-0.5 -right-0.5 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs text-white">
+                  {notifications.length}
+                </span>
+              )}
+              <Avatar user={session.user} />
+            </>
+          }
+        />
       )}
     </div>
   );
@@ -29,12 +43,11 @@ function NavbarIcon({ icon: Icon, to }: { icon: JSX.Element; to: string }) {
   const location = useLocation();
   const isActive =
     to === "/" ? location.pathname === to : location.pathname.startsWith(to);
-  console.log({ Icon });
   return (
     <Link to={to}>
       <div
         className={cx(
-          "flex h-12 w-12 flex-col items-center justify-center rounded-full",
+          "relative flex h-12 w-12 flex-col items-center justify-center rounded-full",
           {
             "bg-blue-500 text-white": isActive,
           }
--- web_mobile/ybook/ybook-client/src/hooks/notifications/useNotifications.tsx
+++ web_mobile/ybook/ybook-client/src/hooks/notifications/useNotifications.tsx
@@ -1,8 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
 
+export interface NotificationsResponse {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  read: boolean;
+  friendshipId: number;
+  conversationMessageId: null;
+  friendship: Friendship;
+  message: null;
+}
+
+export interface Friendship {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  status: string;
+  fromId: number;
+  toId: number;
+}
+
 const NOTIFICATIONS_KEY = () => "/notifications";
 
 export function useNotifications() {
-  const { data, isLoading } = useQuery([NOTIFICATIONS_KEY()]);
+  const { data, isLoading } = useQuery<NotificationsResponse[]>([
+    NOTIFICATIONS_KEY(),
+  ]);
   return [data, isLoading] as const;
 }
