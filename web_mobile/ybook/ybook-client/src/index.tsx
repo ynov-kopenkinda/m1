9091d949d8dac0f98e756271f6da9e6fa1896816
Dmitrii <kopenkin.da@gmail.com>
2023-01-31 21:12:29 +0100

    added automatic sw registering in prod

--- web_mobile/ybook/ybook-client/src/index.tsx
+++ web_mobile/ybook/ybook-client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/globals.css";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { env } from "./env";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -16,4 +17,6 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+env.NODE_ENV === "production"
+  ? serviceWorkerRegistration.register()
+  : serviceWorkerRegistration.unregister();
