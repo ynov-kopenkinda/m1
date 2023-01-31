7481a91554727ffd8fb7e2875fb7b131138b91ef
Dmitrii <kopenkin.da@gmail.com>
2023-01-31 20:52:38 +0100

    Changed avatar provider

--- web_mobile/ybook/ybook-client/src/components/default/Avatar.tsx
+++ web_mobile/ybook/ybook-client/src/components/default/Avatar.tsx
@@ -1,5 +1,5 @@
 import { createAvatar } from "@dicebear/core";
-import { croodlesNeutral } from "@dicebear/collection";
+import { lorelei } from "@dicebear/collection";
 import { DetailedHTMLProps, forwardRef, HTMLAttributes } from "react";
 import cx from "classnames";
 import { useS3Image } from "../../hooks/users/useS3Image";
@@ -16,7 +16,7 @@ export const Avatar = forwardRef<
   const s3avatar = useS3Image(user.avatarS3Key);
   let url: string = s3avatar ?? "";
   if (url === "") {
-    const avatar = createAvatar(croodlesNeutral, {
+    const avatar = createAvatar(lorelei, {
       seed: fullname,
     });
     url = avatar.toDataUriSync();
